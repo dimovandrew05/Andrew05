d07ab85e64b21effe894e6fc1a618030c12472e0
Andrew <dimov-andrew@mail.ru>
2018-06-21 15:23:12 +0300

    Задача №2 решена с подсказки руководителя.

--- study_02.js
+++ study_02.js
@@ -1,7 +1,25 @@
 // Задача № 2
-const myArray = ["John", "Clark"];
 
-item = myArray.map(function(item) {
-    return item.toUpperCase();
-});
-console.log(item);
+const myArray = ["John", "Clark"]
+
+
+function map(array, func) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(func(array[i]));
+    }
+    return newArray;
+}
+
+const myMappedArray = map(myArray, (item) => {
+    return item;
+})
+console.log(myMappedArray)
+
+
+// const myArray = ["John", "Clark"];
+//
+// item = myArray.map(function(item) {
+//     return item.toUpperCase();
+// });
+// console.log(item);
