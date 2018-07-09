f5818f0834c191c2df4c682692c067306b8167f2
Andrew <dimov-andrew@mail.ru>
2018-07-09 16:57:06 +0300

    Удалено

--- study_01.js
+++
@@ -1,9 +0,0 @@
-// Задача №1
-var myArray = [
-    {name: "John", description: "John's description"},
-    {name: "Clark", description: "Clark's description"}
-];
-for(var i=0; i < myArray.length; i++) {
-    console.log(myArray[i].name);
-}
-// console.log(inject);.
